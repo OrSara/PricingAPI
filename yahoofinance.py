91ff42909c7469ea35c4168d780f1fb1a0f49c9c
OrSara <162731615+OrSara@users.noreply.github.com>
2024-05-06 17:00:37 +0100

    Update yahoofinance.py

--- yahoofinance.py
+++ yahoofinance.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 def getRiskFreeRate():
     symbol = "^TNX"
-    start_date = (datetime.now() - timedelta(2)).strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(4)).strftime('%Y-%m-%d')
     end_date = datetime.today().strftime('%Y-%m-%d')
     data = yf.download(symbol, start=start_date, end=end_date)
     riskFreeRate = data["Close"][-1]
